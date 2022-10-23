a63ee4b6efb06b37b8fef7c1d0e181735efbe637
muze <doger229337@yandex.ru>
2022-10-23 18:06:45 +0300

    up

--- src/main/kotlin/Begin/1.kt
+++ src/main/kotlin/Begin/1.kt
@@ -1,11 +1,13 @@
 import java.util.*
-
 fun main() {
-    val scanner = Scanner(System.in)
-    val a = scanner.nextgInt()
-    val p = scanner.nextInt()
-    p=4*a
-    println(p)
+    println("Введите число стороны: ")
+
+    var a:Int
+    var summ:Int
+    a=readLine()!!.toInt()
+    summ=4*a
+    println("Периметр=$summ")
+
 
 
 }
---
+++ src/main/kotlin/Begin/2.kt
@@ -0,0 +1,8 @@
+fun main() {
+println("Введите сторону: ")
+    var a:Int
+    var s:Int
+    a= readLine()!!.toInt()
+    s=a*a
+    println("Площадь равна=$s")
+}
