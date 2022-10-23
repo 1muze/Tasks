a224d0127fb00c239d553e8f157bf60204a50534
muze <doger229337@yandex.ru>
2022-10-23 18:33:33 +0300

    up

---
+++ src/main/kotlin/Begin/5.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val a:Int
+    println("Введите A:")
+    a= readLine()!!.toInt()
+    val b:Int
+    println("Введите B")
+    b= readLine()!!.toInt()
+    println("А=$b")
+    println("B=$a")
+
+
+}
+
