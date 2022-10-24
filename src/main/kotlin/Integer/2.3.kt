b42b6d1ebf2c30609dd361d329837c4f0c3168e2
muze <doger229337@yandex.ru>
2022-10-24 16:32:06 +0300

    up

--- src/main/kotlin/Integer/2.3.kt
+++ src/main/kotlin/Integer/2.3.kt
@@ -2,12 +2,13 @@ fun main() {
     print("Введите число: ")
     var a:Int
     a= readLine()!!.toInt()
-    var d=a/10
-    var d1=a%10
+    var d=a/100 //3
+    var d1=a%100/10 //
+    var d2=a%10
     var sum:Int
-    sum=d+d1
+    sum=d+d1+d2
     var umn:Int
-    umn=d*d1
+    umn=d*d1*d2
     println("Сумма: $sum")
     print("Умножение: $umn")
 
