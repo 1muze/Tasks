6d616c6b9e17f112f9cb863501f21a66935fecc2
muze <doger229337@yandex.ru>
2022-10-27 20:59:36 +0300

    up

---
+++ src/main/kotlin/Boolean/3.6.kt
@@ -0,0 +1,14 @@
+fun main() {
+    print("Число А: ")
+    var a:Int
+    a= readLine()!!.toInt()
+    print("Число B: ")
+    var b:Int
+    b= readLine()!!.toInt()
+    print("Число C: ")
+    var c:Int
+    c= readLine()!!.toInt()
+    var g:Boolean=a>0&&b>0&&c>0
+    println("$g")
+
+}
