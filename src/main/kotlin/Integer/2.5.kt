c91870453ab078c75230a0b6a1b70b9a361fae4c
muze <doger229337@yandex.ru>
2022-10-31 14:34:18 +0300

    up

--- src/main/kotlin/Integer/2.5.kt
+++ src/main/kotlin/Integer/2.5.kt
@@ -4,8 +4,10 @@ fun main() {
     a= readLine()!!.toInt()
     var ch1:Int
     var ch2:Int
+    var ch3:Int
     ch1=a/100
     ch2=a%100
-    println("Перенос 1 цифры в конец: $ch2$ch1")
+    ch3=ch2*10+ch1
+    println("Перенос 1 цифры в конец: $ch3 ")
 
 }
