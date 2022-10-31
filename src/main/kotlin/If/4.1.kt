9369fe417fb05a6adf0bd8766e5c523effa45c24
muze <doger229337@yandex.ru>
2022-10-31 15:47:05 +0300

    up

---
+++ src/main/kotlin/If/4.1.kt
@@ -0,0 +1,12 @@
+fun main() {
+    print("Введите число: ")
+    var a:Int
+    a= readLine()!!.toInt()
+    var b:Int
+    if (a>0){
+        b=a+1
+        println("$b")
+    }
+    else {
+    }
+}
